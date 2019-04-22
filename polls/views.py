55ef8e75fe236e27dc3549b18b24aac7b12d820a
nikitosl <altuxov.nikita@gmail.com>
2019-04-22 23:32:02 +0300

    Начал страницу 40

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,9 +17,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'RESULTS for {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f'VOTE for {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question,
+                                                     'error_message': 'You didnt select a choice'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id, )))
+
+
+
 
