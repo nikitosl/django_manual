c461c0d385f1bbdde7e98d799eec0461e16f4810
Алтухов Никита <altuhov.n.a@ulmrk.ru>
2019-04-30 17:58:03 +0300

    Начал тесты

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+
+
+def create_question(question_text, days):
+    time = timezone.now().date() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_qustion(self):
+        time = timezone.now().date() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_pubkished_recently_with_old_question(self):
+        time = timezone.now().date() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now().date() - (datetime.timedelta(days=1) - datetime.timedelta(seconds=1))
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -10,7 +11,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         '''Return last 5 published questions.'''
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now().date()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
